18d7bbed5d7f922ae20ca1ce90e625c11fb056e9
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-07-21 14:45:09 +0200

    Add functionality

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '479234123574-1k385hnee77mn9lcqr7stnpk80khc0km.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'nktx2P45W08MK0_PmNUnswlr',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
